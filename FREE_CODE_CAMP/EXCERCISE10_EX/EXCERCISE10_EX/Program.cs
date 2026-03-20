829e590b91cb748d34034d60d6bc234935a32cc9
man-donda <mann.9dot@gmail.com>
2026-03-20 09:48:17 +0530

    add more programs

---
+++ FREE_CODE_CAMP/EXCERCISE10_EX/EXCERCISE10_EX/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXCERCISE10_EX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int anglesCount = 3;
+            int[] angles = new int[anglesCount];
+
+            for (int i = 0; i < anglesCount; i++)
+            {
+                Console.WriteLine($"Enter angle {i + 1}: ");
+                angles[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int angleSum = 0;
+
+            foreach (int angle in angles)
+            {
+                angleSum += angle;
+            }
+
+            Console.WriteLine($"The sum of the angles is {angleSum}");
+
+            //if(angleSum == 180)
+            //{
+            //    Console.WriteLine("Valid");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid");
+            //}
+
+            Console.WriteLine(angleSum == 180 ? "Valid" : "Invalid");
+        }
+    }
+}
---
+++ FREE_CODE_CAMP/EXCERCISE9_ARRAY/EXCERCISE9_ARRAY/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXCERCISE9_ARRAY
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[3];
+
+            //Console.WriteLine("Enter number: ");
+            //numbers[0] = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter number: "); 
+            //numbers[1] = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter number: "); 
+            //numbers[2] = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter number: "); 
+            //numbers[3] = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine($"Entered number is {numbers[0]} {numbers[1]} {numbers[2]} {numbers[3]}");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"Enter number {i+1}: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            foreach(int num in numbers)
+            {
+                Console.Write($"{num} ");
+            }
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.Write($"{numbers[i]} ");
+            //}
+            
+            Console.ReadLine();
+        }
+    }
+}
