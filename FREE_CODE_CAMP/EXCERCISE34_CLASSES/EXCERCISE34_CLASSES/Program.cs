50eb92e2975938b333627f08164d6b6becb9172d
man-donda <mann.9dot@gmail.com>
2026-04-01 18:36:30 +0530

    add programs

---
+++ FREE_CODE_CAMP/EXCERCISE34_CLASSES/EXCERCISE34_CLASSES/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXCERCISE34_CLASSES
+{
+    internal class Program
+    {
+        /*
+        struct Person
+        {
+            public string name;
+            public int age;
+
+            public Person(string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+            }
+        }
+        */
+
+        class Person
+        {
+            public string name;
+            public int age;
+
+            public Person()
+            {
+            }
+            public Person(string name)
+            {
+                this.name = name;
+            }
+            public Person(int age)
+            {
+                this.age = age;
+            }
+            public Person(string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+            }
+
+            public string returnDetails()
+            {
+                return $"Name: {name} \nAge: {age}";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Title = "Classes";
+
+            Person person = new Person();
+            Console.WriteLine($"class person: {person.name} - {person.age}");
+            
+            Person person1 = new Person("Aba");
+            Console.WriteLine($"class person1: {person1.name} - {person1.age}");
+            
+            Person person2 = new Person( 23);
+            Console.WriteLine($"class person2: {person2.name} - {person2.age}");
+            
+            Person person3 = new Person("Aba", 23);
+            Console.WriteLine($"class person3: {person3.name} - {person3.age}");
+
+            //---------------------------------------------------------------------------------
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Person person4 = new Person(name, age);
+
+            if (!string.IsNullOrEmpty(person4.name))
+            {
+                Console.WriteLine(person4.name);
+            }
+
+            if(person4.age > -1)
+            {
+                Console.WriteLine(person4.age);
+            }
+            
+//-------------------------------------------------------------------------
+
+            Person person5 = new Person("Aba", 23);
+            //Console.WriteLine($"Name: {person5.name} \nAge: {person5.age}");
+            //Console.WriteLine(person5);
+            Console.WriteLine(person5.returnDetails());
+
+            Console.ReadLine();
+        }        
+    }
+}    
---
+++ FREE_CODE_CAMP/EXCERCISE35_CLASS_FIELDS/EXCERCISE35_CLASS_FIELDS/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXCERCISE35_CLASS_FIELDS
+{
+    internal class Program
+    {
+        class Person
+        {
+            public string name;
+            public int age;
+
+            public Person(string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+            }
+            public void setName(string name)
+            {
+                if (!string.IsNullOrEmpty(name))
+                {
+                    this.name = name;
+                }
+                else
+                {
+                    this.name = string.Empty;
+                }
+            }
+
+            public string returnDetails()
+            {
+                return $"Name: {name} \nAge: {age}";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Person person = new Person("Aba", 23);
+            Console.WriteLine(person.returnDetails());
+
+            //person.name = "Harry";
+            //person.age = 23;
+            person.setName("Harry");
+            Console.WriteLine(person.returnDetails());
+
+            Console.ReadLine();
+        }
+    }
+}
